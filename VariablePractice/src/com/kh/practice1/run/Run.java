9746de4750b73f46edc99c61268840c21b9e914e
potato <aviationlovers@naver.com>
2023-05-25 17:48:39 +0900

    commit

--- VariablePractice/src/com/kh/practice1/run/Run.java
+++ VariablePractice/src/com/kh/practice1/run/Run.java
@@ -9,13 +9,13 @@ public class Run {
 
 	public static void main(String[] args) {
 		VariablePractice1 p1 = new VariablePractice1();
-		//p1.printVariable1();
+		p1.printVariable1();
 		
 		VariablePractice2 p2 = new VariablePractice2();
-		//p2.printVariable2();
+		p2.printVariable2();
 		
 		VariablePractice3 p3 = new VariablePractice3();
-		//p3.printVariable3();
+		p3.printVariable3();
 		
 		VariablePractice4 p4 = new VariablePractice4();
 		p4.printVariable4();
--- VariablePractice1/src/module-info.java
+++
@@ -1,2 +0,0 @@
-module VariablePractice1 {
-}
