ded56376840be220dc5462c66647d1a1919bdeb9
potato <aviationlovers@naver.com>
2023-05-25 17:42:50 +0900

    Update VariablePractice4.java

--- VariablePractice/src/com/kh/practice1/func/VariablePractice4.java
+++ VariablePractice/src/com/kh/practice1/func/VariablePractice4.java
@@ -6,11 +6,10 @@ public class VariablePractice4 {
 	public void printVariable4() {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("문자열을 입력하세요 : ");
-		//char ch = sc.nextLine().charAt(0);
-		System.out.print("첫 번째 문자 : " + sc.nextLine().charAt(0));
-		sc.nextLine();
-		System.out.print("두 번째 문자" + sc.nextLine().charAt(1));
-		System.out.print("세 번째 문자" + sc.nextLine().charAt(2));
+		String str = sc.nextLine();
+		System.out.println("첫 번째 문자 : " + str.charAt(0));
+		System.out.println("두 번째 문자 : " + str.charAt(1));
+		System.out.println("세 번째 문자 : " + str.charAt(2));
 		
 		sc.close();
 		
