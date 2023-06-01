ccdcc6d9db65ca1847186ec578e3a45c8fb02fd2
potato <aviationlovers@naver.com>
2023-06-01 17:12:06 +0900

    commit

--- 03_Control/src/com/kh/chap02/run/LoopRun.java
+++ 03_Control/src/com/kh/chap02/run/LoopRun.java
@@ -26,6 +26,7 @@ public class LoopRun {
 		//a.method16();
 		//a.method17();
 		//a.method18();
+		a.rps();
 		
 		B_While b = new B_While();
 		//b.method1();
@@ -37,7 +38,7 @@ public class LoopRun {
 		C_DoWhile c = new C_DoWhile();
 		//c.method1();
 		//c.method2();
-		c.method3();
+		//c.method3();
 	}
 
 }
