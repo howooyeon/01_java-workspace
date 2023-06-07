7be68f997063534555d319ea4ff2b9339f071be9
potato <aviationlovers@naver.com>
2023-06-07 10:53:00 +0900

    commit

--- 05_Object/src/com/kh/chap03_class/model/vo/Product.java
+++ 05_Object/src/com/kh/chap03_class/model/vo/Product.java
@@ -1,12 +1,59 @@
 package com.kh.chap03_class.model.vo;
 
+// 클래스 선언 구문에 작성가능한 접근제한자(public, default만 가능)
+// default라고 쓰는게 아니고 접근제한자를 지우면 default가 되는 거임
+// default로 하면 같은 패키지 내에서만 사용가능 / 다른 패키지에서는 해당 파일 사용 불가 => 못찾음
+
+
 public class Product {
+		
+	/*
+	 * 필드(field)
+	 * 
+	 * *필드 == 멤버변수 == 인스턴스 변수
+	 * 
+	 * [표현법]
+	 * 접근제한자 [예약어] 자료형 변수명;
+	 * 
+	 * 
+	 */
+	
 	private String pName;
 	private int price;
 	private String brand;
+	
+	/*
+	 * * 생성자
+	 * - 객체를 생성하기 위한 일종의 메소드
+	 * 
+	 * [표현법]
+	 * 접근제한자 클래스명 ([매개변수, 매개변수, 매개변수, ...]){
+	 * 
+	 * 
+	 * }
+	 * 
+	 */
+	
+	
+	/*
+	 * 메소드
+	 * - 기능을 처리하는 담당
+	 * 
+	 * [표현법]
+	 * 접근제한자 [예약어] 반환형 메소드명() {
+	 * // 기능구현
+	 * 
+	 * 
+	 * }
+	 */
+	
+	// 기본생성자
+	public Product() {
+		
+	}
 
-	public void setpName(String pName) {
-		this.pName = pName;
+	public void setpName(String pName) { // 세팅하고 반환할 거 없기 때문에 void
+		this.pName = pName; 
 	}
 
 	public void setPrice(int price) {
@@ -28,5 +75,19 @@ public class Product {
 	public String getBrand() {
 		return brand;
 	}
+	
+	/*
+	 * 내가 생각할 때 유용할 거 같은 메소드 자유롭게 생성 가능
+	 */
+	
+	// 모든 필드값을 다 합친 한 문자열을 반환해주는 메소드
+	public String information() {
+		// return pName, brand, price; return 하나만 됨. 여러개 나열 x 그리고 반환형 정할 수가 없음..
+		
+		return "pName : " + pName + ", " + "price : " + price + ", " + "brand : " + brand;
+		
+	}
+	
+	
 
 }
--- 05_Object/src/com/kh/chap03_class/run/ClassRun.java
+++ 05_Object/src/com/kh/chap03_class/run/ClassRun.java
@@ -1,42 +1,81 @@
 package com.kh.chap03_class.run;
 
+import com.kh.chap03_class.model.vo.Person;
 import com.kh.chap03_class.model.vo.Product;
 
 public class ClassRun {
 
 	public static void main(String[] args) {
 		Product snack = new Product();
-		
-		snack.setpName("참크래커");
-		snack.setBrand("해태");
-		snack.setPrice(1200);
-		
-		System.out.println("===상품1===");
-		System.out.println(snack.getpName());
-		System.out.println(snack.getBrand());
-		System.out.println(snack.getPrice());
-		System.out.println();
 
-		snack.setpName("자두알맹이");
-		snack.setBrand("오리온");
-		snack.setPrice(2000);
+		/*
+		 * snack.setpName("참크래커"); snack.setBrand("해태"); snack.setPrice(1200);
+		 * 
+		 * System.out.println("===상품1==="); System.out.println(snack.getpName());
+		 * System.out.println(snack.getBrand()); System.out.println(snack.getPrice());
+		 * System.out.println();
+		 * 
+		 * snack.setpName("자두알맹이"); snack.setBrand("오리온"); snack.setPrice(2000);
+		 * 
+		 * System.out.println("===상품2==="); System.out.println(snack.getpName());
+		 * System.out.println(snack.getBrand()); System.out.println(snack.getPrice());
+		 * System.out.println();
+		 * 
+		 * snack.setpName("비타오백젤리"); snack.setBrand("광동"); snack.setPrice(1500);
+		 * 
+		 * System.out.println("===상품3==="); System.out.println(snack.getpName());
+		 * System.out.println(snack.getBrand()); System.out.println(snack.getPrice());
+		 */
 		
-		System.out.println("===상품2===");
-		System.out.println(snack.getpName());
-		System.out.println(snack.getBrand());
-		System.out.println(snack.getPrice());
-		System.out.println();
+		Product p1 = new Product();
+		p1.setpName("갤럭시");
+		p1.setPrice(1200000);
+		p1.setBrand("삼성");
+		
+		Product p2= new Product();
+		p2.setpName("아이폰");
+		p2.setPrice(1300000);
+		p2.setBrand("애플");
+		
+		System.out.println("pName : " + p1.getpName() + ", Price : " + p1.getPrice() + ", Brand : " + p1.getBrand());
+		System.out.println("pName : " + p2.getpName() + ", Price : " + p2.getPrice() + ", Brand : " + p2.getBrand());
+		
+		System.out.println("===메소드 만들고 난 후===");
+		System.out.println(p1.information());
+		System.out.println(p2.information());
 		
 		
-		snack.setpName("비타오백젤리");
-		snack.setBrand("광동");
-		snack.setPrice(1500);
+		Person p = new Person();
+		System.out.println(p.getId());
+		System.out.println(p.getAge());
+		System.out.println(p.getGender());
 		
-		System.out.println("===상품3===");
-		System.out.println(snack.getpName());
-		System.out.println(snack.getBrand());
-		System.out.println(snack.getPrice());
+		// 생성 후에 필드에 담긴 값을 곧바로 알아보면
+		// JVM이 초기화(값 세팅)까지 진행해줬음!
 		
+		p.setId("user01");
+		p.setPwd("pwd01");
+		p.setName("이호연");
+		p.setAge(23);
+		p.setGender('F');
+		p.setPhone("010-xxxx-xxxx");
+		p.setEmail("hihihi@naver.com");
+		
+		System.out.println("=== 값 대입후 ===");
+		
+		System.out.println(p.getId());
+		System.out.println(p.getpwd());
+		System.out.println(p.getName());
+		System.out.println(p.getAge());
+		System.out.println(p.getGender());
+		System.out.println(p.getPhone());
+		System.out.println(p.getEamil());
+		
+		
+		System.out.println(p.information());
+		
+		
+
 	}
 
 }
