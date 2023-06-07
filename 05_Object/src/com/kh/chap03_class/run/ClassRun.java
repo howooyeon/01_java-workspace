f7fdd4bec136a9f57ed79a7946abadb8cc354131
potato <aviationlovers@naver.com>
2023-06-07 23:24:40 +0900

    commit

--- 05_Object/src/com/kh/chap03_class/run/ClassRun.java
+++ 05_Object/src/com/kh/chap03_class/run/ClassRun.java
@@ -8,24 +8,36 @@ public class ClassRun {
 	public static void main(String[] args) {
 		Product snack = new Product();
 
-		/*
-		 * snack.setpName("참크래커"); snack.setBrand("해태"); snack.setPrice(1200);
-		 * 
-		 * System.out.println("===상품1==="); System.out.println(snack.getpName());
-		 * System.out.println(snack.getBrand()); System.out.println(snack.getPrice());
-		 * System.out.println();
-		 * 
-		 * snack.setpName("자두알맹이"); snack.setBrand("오리온"); snack.setPrice(2000);
-		 * 
-		 * System.out.println("===상품2==="); System.out.println(snack.getpName());
-		 * System.out.println(snack.getBrand()); System.out.println(snack.getPrice());
-		 * System.out.println();
-		 * 
-		 * snack.setpName("비타오백젤리"); snack.setBrand("광동"); snack.setPrice(1500);
-		 * 
-		 * System.out.println("===상품3==="); System.out.println(snack.getpName());
-		 * System.out.println(snack.getBrand()); System.out.println(snack.getPrice());
-		 */
+		
+		snack.setpName("참크래커");
+		snack.setBrand("해태");
+		snack.setPrice(1200);
+
+		System.out.println("===상품1===");
+		System.out.println(snack.getpName());
+		System.out.println(snack.getBrand());
+		System.out.println(snack.getPrice());
+		System.out.println();
+
+		snack.setpName("자두알맹이");
+		snack.setBrand("오리온");
+		snack.setPrice(2000);
+
+		System.out.println("===상품2===");
+		System.out.println(snack.getpName());
+		System.out.println(snack.getBrand());
+		System.out.println(snack.getPrice());
+		System.out.println();
+
+		snack.setpName("비타오백젤리");
+		snack.setBrand("광동");
+		snack.setPrice(1500);
+
+		System.out.println("===상품3===");
+		System.out.println(snack.getpName());
+		System.out.println(snack.getBrand());
+		System.out.println(snack.getPrice());
+		 
 		
 		Product p1 = new Product();
 		p1.setpName("갤럭시");
