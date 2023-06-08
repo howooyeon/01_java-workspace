f09e0dc3c0125709107085e28e8be5f4efdf1177
potato <aviationlovers@naver.com>
2023-06-08 16:43:48 +0900

    commit

--- 06_ObjectArray/src/com/kh/chap02_objectArray/run/ObjectArrayRun.java
+++ 06_ObjectArray/src/com/kh/chap02_objectArray/run/ObjectArrayRun.java
@@ -1,5 +1,7 @@
 package com.kh.chap02_objectArray.run;
 
+import java.util.Scanner;
+
 import com.kh.chap02_objectArray.model.vo.Phone;
 
 public class ObjectArrayRun {
@@ -16,22 +18,46 @@ public class ObjectArrayRun {
 		}
 
 		Phone[] phones = new Phone[3]; // 객체 생성한 것이 아니고 배열을 생성한 것
-		System.out.println(phones);
-		System.out.println(phones[0]);
 		
+		/*
+		 * System.out.println(phones); System.out.println(phones[0]);
+		 * phones[0].setName("아이폰");
+		 */
+
+		phones[0] = new Phone();
+		phones[1] = new Phone("아이폰", "애플", 13000000, "14pro");
+		phones[2] = new Phone("갤럭시", "삼성", 12000000, "s23");
+		
+		phones[0].setName("벨벳폰");
+		phones[0].setBrand("LG");
+		phones[0].setPrice(100000);
+		phones[0].setSeries("1");
+		
+		int total = 0;
+		for(int i =0; i <phones.length; i++) {
+			System.out.println(phones[i].information());
+			total += phones[i].getPrice();
+		}
+		System.out.println("총가격 : " + total);
+		System.out.println("평균 가격 : " + total / phones.length);
+		
+		System.out.println("=======================================");
+		
+		// 사용자에게 구매하고자 하는 핸드폰명을 입력받아
+		Scanner sc = new Scanner(System.in);
+		System.out.print("구매할 핸드폰 명 : ");
+		String search = sc.nextLine();
+		// 해당 휴대폰을 찾은 후에 그 가격을 알려
+
+		for (int i = 0; i < phones.length; i++) {
+			if (phones[i].getName().equals(search)) {
+				System.out.println(phones[i].information()); //
+			}
+		}
+
+		// 구매하고자 하는 휴대폰 이름 입력 : 벨벳폰
+		// 당신이 구매하고자 하는 휴대폰의 가격은 1000000원 입니다.
+
 	}
 
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 }
---
+++ 06_ObjectArray_Homework/src/com/hw1/run/Run.java
@@ -0,0 +1,53 @@
+package com.hw1.run;
+
+import com.hw1.model.vo.Employee;
+
+public class Run {
+
+	public static void main(String[] args) {
+		// 객체 배열을 크기 3으로 할당 한 뒤
+		Employee[] employees = new Employee[3];
+		
+		// 0번 인덱스에는 기본생성자를 통해서 객체 생성
+		employees[0] = new Employee();
+		System.out.println(employees[0].information());
+		
+		// 1번 인덱스에는 매개변수 6개짜리 생성자를 이용해서 객체 생성
+		Employee employees1 = new Employee(1, "홍길동", 19, 'M', "01022223333", "서울 잠실");
+		System.out.println(employees1.information());
+		
+		// 2번 인덱스에는 매개변수 10개짜리 생성자를 이용하여 객체 생성 후 출력
+		Employee employee2 = new Employee(2, "강말순", "교육부", "강사", 20, 'F', 10000000, 0.01, "01011112222", "서울 마곡");
+		System.out.println(employee2.information());
+		
+		System.out.println("===========================================================================");
+		
+		// 3개의 객체 중 값이 없는 필드에 각각 값을 넣은 뒤 다시 출력(임의로)
+		employees[0] = new Employee(0, "김말똥", "영업부", "팀장", 30, 'M', 3000000, 0.2, "01055559999", "전라도 광주");
+		System.out.println(employees[0].information());
+		employees[1] = new Employee(1, "홍길동", "기획부", "부장", 19, 'M', 4000000 , 0.3, "01022223333", "서울 잠실");
+		
+		System.out.println(employees[1].information());
+		// 직원 각각의 보너스가 적용된 1년 연봉을 계산하여 출력
+		// 보너스가 적용된 연봉 = (급여 +(급여 *보너스 포인트)) * 12
+		
+		int e1Total = (int) ((3000000 + 3000000 * 0.2) * 12);
+		int e2Total = (int) ((4000000 + 4000000 * 0.3) * 12);
+		int e3Total = (int) ((1000000 + 1000000* 0.01) * 12);
+		
+		int totalAverage = (e1Total + e2Total + e3Total)/ 3; 
+		
+		System.out.println("김말똥의 연봉 : " + e1Total);
+		System.out.println("홍길동의 연봉 : " + e2Total);
+		System.out.println("강말순의 연봉 : " + e3Total);
+		
+		System.out.println("===========================================================================");
+		
+		// 3명 직원의 연봉 평균을 구하여 출력
+		System.out.println("직원들의 연봉 평균 : " + totalAverage);
+		
+		
+
+	}
+
+}
