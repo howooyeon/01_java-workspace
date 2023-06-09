ac63520fb77f867a0b574cdc26c26aeffa4d53e6
potato <aviationlovers@naver.com>
2023-06-09 17:49:18 +0900

    commit

---
+++ 07_Inheritance_Homework_이호연/src/com/hw2/model/vo/Circle.java
@@ -0,0 +1,29 @@
+package com.hw2.model.vo;
+
+public class Circle extends Point {
+	private int radius;
+
+	public Circle() {
+
+	}
+
+	public Circle(int x, int y, int radius) {
+		super(x, y);
+		this.radius = radius;
+	}
+
+	public int getRadius() {
+		return radius;
+	}
+
+	public void setRadius(int radius) {
+		this.radius = radius;
+	}
+
+	public void draw() {
+		super.draw();
+		System.out.println("원의 면적 : " + (float)(Math.PI * radius * radius));
+		System.out.println("원의 둘레 : " + (float)(Math.PI * radius * 2));
+	}
+
+}
---
+++ 07_Inheritance_Homework_이호연/src/com/hw2/run/Run.java
@@ -0,0 +1,27 @@
+package com.hw2.run;
+
+import java.util.Iterator;
+
+import com.hw2.model.vo.Circle;
+import com.hw2.model.vo.Rectangle;
+
+public class Run {
+
+	public static void main(String[] args) {
+		// 크기가 2인 Circle, Rectangle 각각 배열 할당
+		Circle[] circle = new Circle[2];
+		circle[0] = new Circle(1,2,3);
+		circle[1] = new Circle(3,3,4);
+		
+		Rectangle[] rectangle = new Rectangle[2];
+		rectangle[0] = new Rectangle(-1,-2,5,2);
+		rectangle[1] = new Rectangle(-2,5,2,8);
+		
+		// 위의 사용 데이터를 참고하여 각각 초기화
+		// 각 도형의 draw 메소드 실행
+		for(int i = 0; i < circle.length; i++){
+			System.out.println(circle.draw);
+		}
+	}
+
+}
