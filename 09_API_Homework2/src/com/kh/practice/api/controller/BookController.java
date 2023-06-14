97b2f942bd43de7965c7b6ef0d356842d746ab97
potato <aviationlovers@naver.com>
2023-06-14 17:45:14 +0900

    commit

---
+++ 09_API_Homework2/src/com/kh/practice/api/controller/BookController.java
@@ -0,0 +1,34 @@
+package com.kh.practice.api.controller;
+
+import com.kh.practice.api.model.vo.Book;
+
+public class BookController {
+	// private 접근제한자로 크기 5의 Book 객체 배열 생성
+	private BookController[] bk = new BookController[5];
+	
+	// 각각의 인덱스에 접근하여 샘플 데이터 넣어서 객체 생성
+	bk[0] = () 
+	
+	public BookController() {
+		
+	}
+	
+	public void printAll() {
+		
+	}
+	
+	public void insertBook(String newTitle, String newAuthor, String newPublisher, String newDate, String newPrice) {
+		
+	}
+	
+	public void printBookpublisherDate() {
+		
+	}
+	
+	public void searchBook(String searchTitle) {
+		
+	}
+	
+	
+
+}
---
+++ 09_API_Homework2/src/com/kh/practice/api/model/vo/Book.java
@@ -0,0 +1,78 @@
+package com.kh.practice.api.model.vo;
+
+import java.util.Date;
+
+public class Book {
+	private String title;
+	private String author;
+	private String publisher;
+	private Date publishDate;
+	private int price;
+	
+	public Book() {
+		
+	}
+	
+	public Book(String title, String author, String publisher, Date publishDate, int price) {
+		this.title = title;
+		this.author = author;
+		this.publisher = publisher;
+		this.publishDate = publishDate;
+		this.price = price;
+	}
+	
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getPublisher() {
+		return publisher;
+	}
+
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+
+	public Date getPublishDate() {
+		return publishDate;
+	}
+
+	public void setPublishDate(Date publishDate) {
+		this.publishDate = publishDate;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "Book [getTitle()=" + getTitle() + ", getAuthor()=" + getAuthor() + ", getPublisher()=" + getPublisher()
+				+ ", getPublishDate()=" + getPublishDate() + ", getPrice()=" + getPrice() + "]";
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
